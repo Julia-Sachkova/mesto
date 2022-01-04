a43966d47bbb2012dda06211483004a73264d88c
Julia Rulova <julia.rulova0066@gmail.com>
2022-01-04 14:09:43 +0300

    fix/js,a,fonts

--- pages/index.js
+++ pages/index.js
@@ -4,29 +4,25 @@ let closeBtn = document.querySelector('.popup__close-button');
 let popup = document.querySelector('.popup');
 let popupOpened = 'popup_opened';
 let submitBtn = document.querySelector('.popup__submit-button');
-
-function openPopup() {
-    popup.classList.add(popupOpened);
-    formValue();
-}
-
-function closePopup() {
-    popup.classList.remove(popupOpened);
-}
-
-profileEditBtn.addEventListener('click', openPopup);
-
-closeBtn.addEventListener('click', closePopup);
-
 let formElement = popup.querySelector('.popup__form');
 let nameInput = popup.querySelector('.popup__text_type_name');
 let jobInput = popup.querySelector('.popup__text_type_about');
 let profileName = document.querySelector('.profile__name');
 let profileAbout = document.querySelector('.profile__about');
 
+function openPopup() {
+    popup.classList.add(popupOpened);
+}
+
+function closePopup() {
+    popup.classList.remove(popupOpened);
+}
+
 function formValue() {
     nameInput.value = profileName.textContent;
     jobInput.value = profileAbout.textContent;
+
+    openPopup();
 }
 
 function formSubmitHandler(evt) {
@@ -38,5 +34,9 @@ function formSubmitHandler(evt) {
 
     closePopup();
 }
+
+profileEditBtn.addEventListener('click', openPopup);
+
+closeBtn.addEventListener('click', closePopup);
 
 formElement.addEventListener('submit', formSubmitHandler);
